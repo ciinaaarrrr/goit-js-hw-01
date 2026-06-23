1d5779c7d48ddae5a0663ca7b87839b838ab2a8c
JuniorKabakistan <juniorkabakistan@gmail.com>
2026-06-23 18:05:39 +0300

    taskt-1 test

--- task-1.js
+++ task-1.js
@@ -1,1 +1,8 @@
+"use strich";
 
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth < totalPrice > credits!`;
+}
+
+console.log(makeTransaction(5, 3000));
